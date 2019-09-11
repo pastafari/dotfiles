379d8e0faf6b253a066089765fcbdbfe6976301e
Mohit Thatte <mohit.thatte@gmail.com>
2019-09-11 23:32:22 +0200

    Add a bunch of guard clauses

--- go.sh
+++ go.sh
@@ -1,7 +1,23 @@
 #! /usr/bin/env bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+function info {
+    echo -e "${GREEN}$1${NO_COLOR}"
+}
+
+function danger {
+    echo -e "${RED}$1${NO_COLOR}"
+}
+
 function install_homebrew {
-    command -v brew >/dev/null 2>&1
+    if ! [ -x "$(command -v brew)" ]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        info "Homebrew already installed"
+    fi
 }
 
 function install_dev_essentials {
@@ -9,26 +25,41 @@ function install_dev_essentials {
 }
 
 function setup_emacs {
+    info "Setting up emacs for you pastafari"
+    
     # Change to ~ to clone emacs.d
-    pushd ~/
+    pushd ~/ > /dev/null
+
+    # Guard for existing emacs.d
+    if [ -d ".emacs.d" ]; then
+       danger "~/.emacs.d already exists. Overwrite? [y/n]"
+
+       read overwrite
+
+       [[ "$overwrite" == "n" ]] || info "Ok. Not cloning pastafari/prelude"; return 0
+    fi
+
+    info "Cloning and setting up pastafari/prelude with branch pastafari"
 
     git clone git@github.com:pastafari/prelude.git .emacs.d
     cd .emacs.d
     git checkout pastafari
 
     # Change back to where we were before
-    popd
+    popd > /dev/null
 }
 
 # Install Homebrew
-echo "Installing Homebrew"
+info "Installing Homebrew"
 install_homebrew
 
 # Install dev essentials defined in dev_essentials/Brewfile
-echo "Installing dev essentials"
+info "Installing dev essentials"
 install_dev_essentials
 
 # Set up Emacs
-echo "Setting up Emacs"
+info "Setting up Emacs"
 setup_emacs
 
+
+info "Done"
