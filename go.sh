2221ec49eb474e95379dde99fc74040667d38447
Mohit Thatte <mohit.thatte@gmail.com>
2019-09-11 22:47:37 +0200

    Setup emacs
    - clone prelude and switch to pastafari branch

--- go.sh
+++ go.sh
@@ -4,10 +4,26 @@ function install_homebrew {
     command -v brew >/dev/null 2>&1
 }
 
+function install_dev_essentials {
+    brew bundle install --file=./dev_essentials/Brewfile
+}
+
+function setup_emacs {
+    cd ~/
+    "Cloning"
+    git clone git@github.com:pastafari/prelude.git .emacs.d
+    cd .emacs.d
+    git checkout pastafari
+}
+
 # Install Homebrew
 echo "Installing Homebrew"
 install_homebrew
 
 # Install dev essentials defined in dev_essentials/Brewfile
 echo "Installing dev essentials"
-brew bundle install --file=./dev_essentials/Brewfile
+install_dev_essentials
+
+# Set up Emacs
+echo "Setting up Emacs"
+setup_emacs
